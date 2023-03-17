1ae7d7036fb646e33a5fff5d8482b1f2b9c186ac
Celeste Bareiro <cnyx7@proton.me>
2023-03-17 18:50:06 +0000

    second commit, working on Fetch and API

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,34 +1,86 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../../styles/index.css";
 
 const Home = () => {
-	const [ inputValue, setInputValue] = useState("");
-	const [ toDos, settoDos ] = useState([]);
+	const [inputValue, setInputValue] = useState("");
+	const [toDos, settoDos] = useState([]);
+
+	const fetchToDoList = async () => {
+		try {
+			const response = await fetch("/todos/user/username");
+			const data = await response.json();
+			settoDos(data);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	useEffect(() => {
+		fetchToDoList();
+	}, []);
+
+	const updateToDoList = async (newToDoList) => {
+		try {
+			const response = await fetch("/todos/user/username", {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify(newToDoList)
+			});
+			const data = await response.json();
+			console.log(data);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	const handleAddToDo = (newToDo) => {
+		const newToDoList = toDos.concat([newToDo]);
+		settoDos(newToDoList);
+		updateToDoList(newToDoList);
+	};
+
+	const handleDeleteToDo = (index) => {
+		const newToDoList = toDos.filter((_, currentIndex) => index !== currentIndex);
+		settoDos(newToDoList);
+		updateToDoList(newToDoList);
+	};
+
+	const handleDeleteAllToDos = () => {
+		settoDos([]);
+		updateToDoList([]);
+	};
+
 	return (
 		<>
 			<div className="container-fluid row d-flex justify-content-center align-items-center p-2">
 				<h1 className="row justify-content-center">To-Do List</h1>
 				<ul className="list-container col-12">
 					<li className="list-title">
-						<input 
-						className="text" 
-						onChange={(e) => setInputValue(e.target.value)} 
-						value={inputValue}
-						onKeyPress={(e) => {
-							if (e.key === "Enter") {
-								settoDos(toDos.concat([inputValue]));
-								setInputValue("");
-							}
-						}}
-						placeholder="What do you need to do today?">
-						</input>
+						<input
+							className="text"
+							onChange={(e) => setInputValue(e.target.value)}
+							value={inputValue}
+							onKeyPress={(e) => {
+								if (e.key === "Enter" && !/^\s*$/.test(inputValue)) {
+									handleAddToDo(inputValue);
+									setInputValue("");
+								}
+							}}
+							placeholder="What do you need to do today?"
+						/>
+
 					</li>
 					{toDos.map((item, index) => (
 						<li className="list-item">{item}
-						<button className="ocultar btn btn-sm" type="button" onClick={() => settoDos(toDos.filter((item, currentIndex) => index != currentIndex))}><i className="fas fa-trash-alt"></i></button>
+							<button className="ocultar btn btn-sm" type="button" onClick={() => handleDeleteToDo(index)}><i className="fas fa-trash-alt"></i></button>
 						</li>
 					))}
 					<li className="list-item text-end"><em>{toDos.length} item(s) left.-</em></li>
+					<li className="list-item text-end">
+						<button className="btn btn-sm btn-danger" type="button" onClick={handleDeleteAllToDos}>Delete All</button>
+					</li>
 				</ul>
 			</div>
 		</>
